92195448b1eb28da432553ddbb435646b1ba5398
Eric Workman <ericsparkeworkman+github@gmail.com>
2023-11-16 12:19:54 -0500

    Add validations to loot_item

--- app/models/loot_item.rb
+++ app/models/loot_item.rb
@@ -1,3 +1,6 @@
 class LootItem < ApplicationRecord
   belongs_to :loot_table
+
+  validates :name, presence: true
+  validates :probability, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
